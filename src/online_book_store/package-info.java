57528cd85018402b94bbe2dd9e2758bb175fa0fd
Shashank Jha <shashank13115032359@gmail.com>
2026-03-19 00:59:06 +0530

    design requirements

---
+++ src/online_book_store/package-info.java
@@ -0,0 +1,23 @@
+package online_book_store;
+
+// 2) Design a system like Amazon Books where users can search, view, purchase, review, and rate books online.
+//
+//📚 Subparts to Design
+//
+//* Book Catalog (search, filter, categories)
+//
+//* User Account (signup, login, user roles)
+//
+//* Shopping Cart and Checkout
+//
+//* Payment Integration
+//
+//* Order Management
+//
+//* Book Reviews & Ratings
+//
+//* Inventory Management
+//
+//* Admin Dashboard (add/update/delete books)
+//
+//* Wishlist and Recommendations (optional)
